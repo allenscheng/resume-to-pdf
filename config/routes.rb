1a6dfb2c6fc29b9821e0f66a6f5d341b1e752fe2
Allen Cheng <allencheng219@gmail.com>
2018-01-09 19:33:59 -0600

    added pdf controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/pdf" => "pdf_resumes#show"
+  # get "/test" => "pdf_resumes#test"
 end
